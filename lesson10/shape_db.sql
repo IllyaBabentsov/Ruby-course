65692ba89ab492b4714610c6a9ab7422c4009bd5
IllyaBabentsov <illyababentsov@gmail.com>
2016-09-14 14:28:28 +0300

    Add files via upload
    sql file

---
+++ lesson10/shape_db.sql
@@ -0,0 +1,122 @@
+-- MySQL dump 10.13  Distrib 5.7.13, for Linux (i686)
+--
+-- Host: localhost    Database: shape_db
+-- ------------------------------------------------------
+-- Server version	5.7.13-0ubuntu0.16.04.2
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `shape_db`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `shape_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `shape_db`;
+
+--
+-- Table structure for table `error`
+--
+
+DROP TABLE IF EXISTS `error`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `error` (
+  `type_error` int(11) DEFAULT NULL,
+  `description` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `error`
+--
+
+LOCK TABLES `error` WRITE;
+/*!40000 ALTER TABLE `error` DISABLE KEYS */;
+/*!40000 ALTER TABLE `error` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `figures`
+--
+
+DROP TABLE IF EXISTS `figures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `figures` (
+  `id` int(11) DEFAULT NULL,
+  `figure_name` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `figures`
+--
+
+LOCK TABLES `figures` WRITE;
+/*!40000 ALTER TABLE `figures` DISABLE KEYS */;
+/*!40000 ALTER TABLE `figures` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `formulas`
+--
+
+DROP TABLE IF EXISTS `formulas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `formulas` (
+  `parametr` varchar(20) DEFAULT NULL,
+  `formula` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `formulas`
+--
+
+LOCK TABLES `formulas` WRITE;
+/*!40000 ALTER TABLE `formulas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `formulas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `squares`
+--
+
+DROP TABLE IF EXISTS `squares`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `squares` (
+  `rezult` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `squares`
+--
+
+LOCK TABLES `squares` WRITE;
+/*!40000 ALTER TABLE `squares` DISABLE KEYS */;
+/*!40000 ALTER TABLE `squares` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-09-14  7:20:03
